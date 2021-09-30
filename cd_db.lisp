9de608344c126ad0def2c76079bd714a77a73460
Angel <a.v.iliev13@gmail.com>
2021-09-30 13:08:27 +0300

    Add loading & saving of db to file

--- cd_db.lisp
+++ cd_db.lisp
@@ -6,23 +6,28 @@
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
 
+(defun save-db (filename)
+  (with-open-file (out filename
+                   :direction :output ; Direction -> output - means the file is open for writing
+                   :if-exists :supersede) ;If-exists -> supersede - means that file will be overwritten if it exists
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
 ;; (add-record (make-cd "Paris in the rain" "Lauv" 8 t))
-;; (add-record (make-cd "You need me, I don't need you" "Ed Sheeran" 10 t))
-;; (add-record (make-cd "Lover" "Taylor Swift" 7 t))
-
-;; (format t "DB ~a ~%" *db*)
-
-(print-db)
+;; (print-db)
 
 (defun prompt-read (prompt)
   (format *query-io* "~a: " prompt) ; *query-io* is a global var - contains input stream connected to the terminal
   (force-output *query-io*) ; Necessary so lisp doesnt wait for newline before it displays the prompt
   (read-line *query-io*))
-
-;; (format t "Hello, ~a ~%" (prompt-read "Whats your name"))
 
 (defun prompt-for-cd ()
   (make-cd
@@ -36,5 +41,9 @@
   (loop (add-record (prompt-for-cd))
       (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
 
-(add-cds)
+;; (add-cds)
+;; (save-db (prompt-read "Filename to store DB: "))
+;; (save-db "~/Documents/lisp/db.txt")
+(load-db "~/Documents/lisp/db.txt")
+(format t "~a~%" *db*)
 
